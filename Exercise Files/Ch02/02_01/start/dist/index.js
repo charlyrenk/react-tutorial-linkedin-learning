a9081752c33a9b20c615ddf10a59116898a90bc9
Charles Renk <crenk@periscope.com>
2018-05-14 11:46:04 -0500

    initial file layout for linkedin react tutorial

---
+++ Exercise Files/Ch02/02_01/start/dist/index.js
@@ -0,0 +1,24 @@
+// const { createElement } = React
+const { render } = ReactDOM
+
+const style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'verdana'
+}
+
+// const title = React.createElement(
+//     'h1',
+//     {id: 'title', className: 'header', style: style},
+//     'Hello World'
+// )
+
+// js-X render
+render(
+    <h1 id="title"
+        className='header'
+        style={style}>
+        Hello World
+    </h1>,
+    document.getElementById('react-container')
+)
